7aba0d5d17cb2914203bf56dd62a56eec62456d1
Abdul Rafay <rajpootrafay96@gmail.com>
2025-05-29 13:08:59 +0500

    now created app vuejs

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import Header from './components/header/Header.vue'
+</script>
+
+<template>
+  <header>
+   
+
+    <div class="wrapper">
+        <Header />
+    </div>
+  </header>
+
+  <main>
+  </main>
+</template>
+
+
